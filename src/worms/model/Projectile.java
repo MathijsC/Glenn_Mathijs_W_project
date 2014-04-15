eed3697dfa6aafeb62003b2208504be038f5a225
MathijsC <mathijs.cuppens@gmail.com>
2014-04-15 15:55:56 +0200

    Projectile docu and vragen

--- src/worms/model/Projectile.java
+++ src/worms/model/Projectile.java
@@ -6,38 +6,110 @@ import be.kuleuven.cs.som.annotate.Basic;
 import be.kuleuven.cs.som.annotate.Immutable;
 import be.kuleuven.cs.som.annotate.Raw;
 
-public class Projectile extends Entity {
-	
-	//TODO docu Constructor
-	//TODO Class Invar
-	//TODO DOCU check
+/**
+ * A class of projectiles used in the game worms with a position, world
+ * weapon, direction, mass and force.
+ * 
+ * @invar	The direction of this projectile should be a valid direction at all time.
+ * 			|isValidDirection(getDirection())
+ * @invar	The radius of this projectile should be a valid radius at all time.
+ * 			|isValidRadius(getRadius())
+ * @invar	This projectile should at all time have a weapon.
+ * 			|hasWeapon()
+ * @invar	This projectile should at all time have a valid force.
+ * 			|isValidForce(getForce())
+ * @author 	Glenn Cools & Mathijs Cuppens
+ * @version	1.4
+ */
 
+public class Projectile extends Entity {
+
+	/**
+	 * Initialize this new projectile with the given position, world, weapon
+	 * direction, mass and force.
+	 * 
+	 * @param 	position
+	 * 			The position of this new projectile (an object position holding coordinates).
+	 * @param 	world
+	 * 			The world of this new projectile.
+	 * @param 	weapon
+	 * 			The weapon of this new projectile.
+	 * @param 	direction
+	 * 			The direction of this new projectile.
+	 * @param 	mass
+	 * 			The mass of this new projectile.
+	 * @param 	force
+	 * 			The force of this new projectile.
+	 * @effect	This projectile is initialize as and subobject of the class Entity
+	 * 			with the given position and world.
+	 * 			| super(position,world)
+	 * @effect	The direction of this new projectiile is equal to the given
+	 * 			direction modulo 2*PI.
+	 * 			| setDirection(direction)
+	 * @post	The mass of this new projectile is set to the given mass.
+	 * 			| new.getMass() == mass
+	 * @post	The force of this new projectile is set to the given force.
+	 * 			| new.getForce() == force
+	 * @post	The weapon of this new projectile is set to the given weapon.
+	 * 			| new.getWeapon() == weapon
+	 */
 	public Projectile(Position position, World world, Weapon weapon,
 			double direction, double mass, double force) {
-		super(position,world);
+		super(position, world);
 		this.setDirection(direction);
 		this.mass = mass;
 		this.force = force;
 		this.setWeapon(weapon);
 	}
 
-
-	/** Returns the weapon that created this projectile
-	 * @return the weapon
+	/** 
+	 * Returns the weapon that created this projectile.
+	 * 
+	 * @return 	The weapon of this projectile.
 	 */
 	public Weapon getWeapon() {
 		return weapon;
 	}
 
-	/** Sets the weapon that created this projectile
-	 * @param weapon the weapon to set
+	/** 
+	 * Sets the weapon that created this projectile.
+	 * 
+	 * @param 	weapon 
+	 * 			The weapon to set, for this projectile.
 	 */
-	private void setWeapon(Weapon weapon) {
+	private void setWeapon(Weapon weapon) throws IllegalArgumentException,IllegalStateException{
+		if (hasWeapon()){
+			throw new IllegalStateException();
+		}
+		if (!canHaveAsWeapon(weapon)){
+			throw new IllegalArgumentException();
+		}
 		this.weapon = weapon;
+	}
+	
+	/**
+	 * Return true if this projectile has a weapon.
+	 * 
+	 * @return True if this projectile  has a weapon.
+	 */
+	public boolean hasWeapon(){
+		return getWeapon() != null;
+	}
+	
+	/**
+	 * Return true if the projectile can have the given weapon as a weapon.
+	 * 
+	 * @param 	weapon
+	 * 			The weapon to check if this projectile can have this as weapon.
+	 * @return	True if the projectile can have the given weapon as a weapon.
+	 * 			| weapon != null
+	 */
+	public boolean canHaveAsWeapon(Weapon weapon){
+		return weapon != null;
 	}
 
 	/**
-	 * Variablie to register the weapon that created this projectile
+	 * Variable to register the weapon that created this projectile.
 	 */
 	private Weapon weapon;
 
@@ -47,7 +119,7 @@ public class Projectile extends Entity {
 	private double direction;
 
 	/**
-	 * Return true if the given direction is a valid direction
+	 * Returns true if the given direction is a valid direction.
 	 * 
 	 * @param 	direction
 	 * 			The direction to check whether it is a valid one.
@@ -61,7 +133,7 @@ public class Projectile extends Entity {
 	}
 
 	/**
-	 * Return the direction of this projectile.
+	 * Returns the direction of this projectile.
 	 * 
 	 * @return	The direction of this projectile.
 	 */
@@ -72,10 +144,10 @@ public class Projectile extends Entity {
 	}
 
 	/**
-	 * Set the direction of this projectile to the given direction
+	 * Set the direction of this projectile to the given direction.
 	 * 
 	 * @param 	direction
-	 * 			The new direction of this projectile
+	 * 			The new direction of this projectile.
 	 * @post	If the new direction of this projectile is positive after calculated
 	 * 			module 2*PI, the direction is added to the base (2*PI) to get
 	 * 			a positive direction between 0 and 2*PI.
@@ -92,9 +164,9 @@ public class Projectile extends Entity {
 		else
 			this.direction = (direction % (Math.PI * 2));
 	}
-	
+
 	/**
-	 *  Variable to register the mass of this projectile
+	 *  Variable to register the mass of this projectile.
 	 */
 	final double mass;
 
@@ -108,9 +180,9 @@ public class Projectile extends Entity {
 	public double getMass() {
 		return this.mass;
 	}
-	
+
 	/**
-	 * Variable to register the force exerted on this projectile
+	 * Variable to register the force exerted on this projectile.
 	 */
 	final double force;
 
@@ -126,7 +198,18 @@ public class Projectile extends Entity {
 	}
 	
 	/**
-	 * Variable to register the radius of this projectile
+	 * Returns true is the given force is a valid amount of force.
+	 * 
+	 * @param 	force
+	 * 			The force to check if it is valid.
+	 * @return	True if the force is not negative, not zero, not NaN and not infinity.
+	 */
+	public static boolean isValidForce(double force){
+		return ((force >=0) && (force != Double.NaN) && (force != Double.POSITIVE_INFINITY));
+	}
+
+	/**
+	 * Variable to register the radius of this projectile.
 	 */
 	final double radius = calcRadius();
 
@@ -157,7 +240,7 @@ public class Projectile extends Entity {
 	 * @param 	radius
 	 * 			The radius to check whether it is a valid one.
 	 * @return	True if the given radius is valid.
-	 * 			| radius >= getMinRadius();
+	 * 			| radius >= getMinRadius()
 	 */
 	@Raw
 	public static boolean isValidRadius(double radius) {
@@ -167,8 +250,6 @@ public class Projectile extends Entity {
 	/** 
 	 * Calculates the radius of this projectile based on its density and mass.
 	 * 
-	 * @param	Mass
-	 * 			The mass of this projectile to calculate the radius of this projectile.
 	 * @return	Returns the radius of this projectile based on its density and mass.
 	 * 			| VOLUME = MASS/DENSITY
 	 * 			| VOLUME = 4/3*PI*R^3
@@ -203,20 +284,19 @@ public class Projectile extends Entity {
 	/** 
 	* Let this projectile jump over a distance.
 	* 
-	* If the projectile is still alive:
-	* @effect A theoretical jump will be calculated to get the location where it will hit something (worm or terrain)
-	* 			and the projectile's location will be set to the new location
+	* @Post 	If this projectile is not terminated, the function will do it's work.
+	* 			| if(!isTerminated)
+	* 			|	then proceed
+	* @effect 	A theoretical jump will be calculated to get the location where it will hit something (worm or terrain)
+	* 			and this projectile's location will be set to the new location
 	* 			| new.getPosition = possibleJump()
-	* @effect  If the new location is on a worm the projectile will deal damage to that worm
+	* @effect  	If the new location is on a worm this projectile will deal damage to that worm
 	* 			and it will be destroyed
-	* 				| if(wormHit)
-	* 				| then dealDamage() and terminate()
-	* 
-	* @post If the projectile doesn't hit a worm it will be destroyed
-	* 				| if(!wormHit)
-	* 				| then terminate()
-	* If the projectile is already terminate:
-	* @Post nothing happens
+	* 			| if(wormHit)
+	* 			| 	then dealDamage() and terminate()
+	* @post 	If this projectile doesn't hit a worm it will be destroyed
+	* 			| if(!wormHit)
+	* 			| 	then terminate()
 	*/
 
 	public void jump(double timeStep) {
@@ -247,23 +327,23 @@ public class Projectile extends Entity {
 
 	}
 
-	/** A theoretical jump will be performed to determine the location where it will hit something
-	 * 			and to calculate the time it will take to perform that jump
+	/** 
+	 * A theoretical jump will be performed to determine the location where it will hit something
+	 * and to calculate the time it will take to perform that jump
 	 * 
-	 * The function will calc step by step the next location on the trajectory of this projectile
-	 * 				and will check if the location is passable or if the projectile will hit a worm
-	 * 				at that location, 
-	 * 				if so the function will stop and will return the final location of the jump, 
-	 * 				if not the new position will be stored in a local variable and the next position
-	 * 				will be calculated
+	 * The function will calculate step by step the next location on the trajectory of this projectile
+	 * and will check if the location is passable or if the projectile will hit a worm
+	 * at that location, 
+	 * if so the function will stop and will return the final location of the jump, 
+	 * if not the new position will be stored in a local variable and the next position
+	 * will be calculated.
 	 * 
-	 * @param timeStep An elementary time interval during which you may assume
-	 *                 that the projectile will not completely move through a piece of impassable terrain.
-	 * @return	Returns The location where the jump will end (by hitting anything)
-	 * 					 and the time it will take to perform that jump
+	 * @param 	timeStep 
+	 * 			An elementary time interval during which you may assume that this projectile will not completely move through a piece of impassable terrain.
+	 * @return	Returns The location where the jump will end (by hitting anything) and the time it will take to perform that jump
 	 */
 	public double[] possibleJump(double timeStep) {
-	
+
 		Position position = this.getPosition();
 		double time = timeStep;
 		Position tempPosition;
@@ -273,8 +353,8 @@ public class Projectile extends Entity {
 		while ((jumping) && (!hit)) {
 			tempPosition = this.jumpStep(time);
 			if (getWorld().isPassable(tempPosition.getXCoordinate(),
-					tempPosition.getYCoordinate(),this.getRadius())) {
-				position = tempPosition;				
+					tempPosition.getYCoordinate(), this.getRadius())) {
+				position = tempPosition;
 				time = time + timeStep;
 				if (this.getWorld().checkProjectileHitWorm(position,
 						this.getRadius())) {
@@ -303,15 +383,19 @@ public class Projectile extends Entity {
 	 * 			The given time is not during the jump.
 	 * 			| time <=0
 	 * 			| time > this.jumpTime()
+	 * @thorws	IllegalStateException
+	 * 			This projectile is terminated
+	 * 			| isTerminated()
 	 */
-	public Position jumpStep(double time) throws IllegalArgumentException,IllegalStateException {
+	public Position jumpStep(double time) throws IllegalArgumentException,
+			IllegalStateException {
 		if (time <= 0) {
 			throw new IllegalArgumentException();
 		}
 		if (isTerminated()) {
 			throw new IllegalStateException();
 		}
-		// jumpTime vraag een argument dat hier niet gegeven is ... :/
+		// TODO vraag aan assistent en controleer docu @throws erna
 		/*
 		 * if (time > this.jumpTime()) { throw new IllegalArgumentException(); }
 		 */
